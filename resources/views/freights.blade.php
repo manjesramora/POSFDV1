c188775b507cfe7d6d52de51e30d86cdfba99ed3
DiegoMartinez115 <diego.mtz.molina115@gmail.com>
2024-09-23 09:36:20 -0600

    misc: Cambio de titulo de pestañas
    Se cambio el titulo de la pestaña del navegador en las vistas: fletes, permisos, rcn y roles.

--- resources/views/freights.blade.php
+++ resources/views/freights.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>ADMIN DASH</title>
+    <title>Fletes</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
